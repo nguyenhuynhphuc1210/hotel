fc950a85f986765a93972f7fa0f26139571e49f2
Nguyen Huynh Phuc <nguyenhuynhphuc1210@gmail.com>
2026-06-02 17:43:15 +0700

    Fix timezone 2

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,14 +1,23 @@
 package com.example.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class BackendApplication {
+
+	@PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
